b076b058716b8202c7a256d8554fd4b9cb1ebbc3
Paul Cauchois <paul.cauchois@univ-rouen.fr>
2022-12-18 00:27:12 +0100

    Added multiprocessing to testing programs. Made logs.py actually extract the info from log files.

--- Testing.py
+++ Testing.py
@@ -1,77 +1,101 @@
 from methods import *
+import multiprocessing as mp
 import os
+import sys
 
-player = Wordzee("French ODS dictionary.txt", "letters.txt")
 
-method = player.KPPV_search
-logdir = "D://Wordzee/logs/" + method.__name__
-# Number of files in log folder
-n = len([entry for entry in os.listdir(logdir) if
-         os.path.isfile(os.path.join(logdir, entry)) and entry.startswith(method.__name__)]) + 1
-
-param1 = 5
-param2 = 0.5
-for testnum in range(200001-n+1):
-    f = open(os.path.join(logdir, method.__name__ + '_' + str(n + testnum) + '.txt'), 'w')
-    f.write(f"\tSimulation #{n + testnum}\n")
-    f.write(f"{method.__code__.co_varnames[1]} : {param1}\n")
-    if param2:
-        f.write(f"{method.__code__.co_varnames[2]} : {param2}\n")
-    # I know it's bad practice, but I needed the name and __code__ is *right there*.
-    f.write(f"board :\n")
-    player.create_board()
-    for row in player.board:
-        f.write(f"{'|'.join([case if case != '' else '  ' for case in row])}|\n")
-    plays = []
-    points = 0
-    by_line = []
-    wordzee = True
-    for i, row in enumerate(player.board):
-        f.write("----------\n")
-        f.write(f"\tRound {i + 1}\n")
-        print(f"Playing round {row}")
-        k = len(row)
-        player.cases = row
-        player.swap_letters('', inPlace=True)
-        print(f"Letters are {player.letters}")
-        f.write(f"Starting letters : |{'|'.join(player.letters)}|\n")
-        for l in range(2):
-            if param2:
-                kept = method(param1, param2)
+# os.path.join(logdir, method.__name__ + '_' + str(n + testnum) + '.txt')
+def play_game(player, method, filename, number, param1, param2=None, start=0):
+    for testnum in range(number):
+        f = open(filename + str(start + testnum) + '.txt', 'w')
+        f.write(f"\tSimulation #{start + testnum}\n")
+        f.write(f"{method.__code__.co_varnames[1]} : {param1}\n")
+        if param2:
+            f.write(f"{method.__code__.co_varnames[2]} : {param2}\n")
+        # I know it's bad practice, but I needed the name and __code__ is *right there*.
+        f.write(f"board :\n")
+        player.create_board()
+        for row in player.board:
+            f.write(f"{'|'.join([case if case != '' else '  ' for case in row])}|\n")
+        plays = []
+        points = 0
+        by_line = []
+        wordzee = True
+        for i, row in enumerate(player.board):
+            f.write("----------\n")
+            f.write(f"\tRound {i + 1}\n")
+            # print(f"Playing round {row}")
+            k = len(row)
+            player.cases = row
+            player.swap_letters('', inPlace=True)
+            # print(f"Letters are {player.letters}")
+            f.write(f"Starting letters : |{'|'.join(player.letters)}|\n")
+            for l in range(2):
+                if param2:
+                    kept = method(param1, param2)
+                else:
+                    kept = method(param1)
+                # print(f"Keeping {kept}")
+                player.swap_letters(kept)
+                # print(f"New letters are {player.letters}")
+                f.write(f"Swap {l + 1} :           |{'|'.join(player.letters)}|\n")
+            P = player.words_containing(player.letters, row)
+            played = max(P.keys(), key=P.get)
+            if len(played) < k:
+                wordzee = False
+                plays.append(list(played) + (k - len(played)) * [' '])
             else:
-                kept = method(param1)
-            print(f"Keeping {kept}")
-            player.swap_letters(kept)
-            print(f"New letters are {player.letters}")
-            f.write(f"Swap {l + 1} :           |{'|'.join(player.letters)}|\n")
-        P = player.words_containing(player.letters, row)
-        played = max(P.keys(), key=P.get)
-        if len(played) < k:
-            wordzee = False
-            plays.append(list(played) + (k - len(played)) * [' '])
-        else:
-            plays.append(list(played))
-            P[played] -= player.full_bonus
-        P[played] *= i + 1
-        print(f"Playing {played} for {P[played]} points")
-        print(played)
-        f.write(f"{' |'.join(plays[-1])} | {P[played]}\n")
-        f.write(f"{'|'.join([case if case != '' else '  ' for case in row])}|\n")
-        points += P[played]
-        by_line.append(P[played])
-        print("-----------")
-    if wordzee:
-        points += 100
-        print("Wordzee !")
+                plays.append(list(played))
+                P[played] -= player.full_bonus
+            P[played] *= i + 1
+            # print(f"Playing {played} for {P[played]} points")
+            # print(played)
+            f.write(f"{' |'.join(plays[-1])} | {P[played]}\n")
+            f.write(f"{'|'.join([case if case != '' else '  ' for case in row])}|\n")
+            points += P[played]
+            by_line.append(P[played])
+            # print("-----------")
+        if wordzee:
+            points += 100
+            # print("Wordzee !")
 
-    f.write(f"----------\n")
-    for i, p in enumerate(plays):
-        f.write(f"{'|'.join(p)}| {'  ' * (player.max_letters - len(p))} |{by_line[i]}|\n")
+        f.write(f"----------\n")
+        for i, p in enumerate(plays):
+            f.write(f"{'|'.join(p)}| {'  ' * (player.max_letters - len(p))} |{by_line[i]}|\n")
 
-    f.write(f"\nWordzee : {wordzee}\n")
-    f.write(f"Total score : {points}")
+        f.write(f"\nWordzee : {wordzee}\n")
+        f.write(f"Total score : {points}")
 
-    print(plays)
-    print(points)
+        print(f"Game number {start + testnum}\nPoints:{points}\nWordzee:{wordzee}")
 
-    f.close()
+        # print(plays)
+        # print(points)
+
+        f.close()
+
+
+def simuls(start, number):
+    player = Wordzee("French ODS dictionary.txt", "letters.txt")
+
+    method = player.KPPV_search
+    logdir = "D://Wordzee/logs/" + method.__name__
+    # Number of files in log folder
+    n = len([entry for entry in os.listdir(logdir) if
+             os.path.isfile(os.path.join(logdir, entry)) and entry.startswith(method.__name__)]) + 1
+
+    param1 = 2
+    param2 = 1 / 2 - 0.001  # minus epsilon for stability.
+    play_game(player, method, os.path.join(logdir, method.__name__ + '_'), number, param1, param2, start=start)
+
+
+if __name__ == '__main__':
+    free_cores = 1
+    d = "D://Wordzee/logs/" + "KPPV_search"
+    s = len([entry for entry in os.listdir(d) if
+             os.path.isfile(os.path.join(d, entry))]) + 1
+    max_cores = mp.cpu_count()
+    cores = max_cores - free_cores
+    nmax = 100000
+    processes = [mp.Process(target=simuls, args=(s + k * nmax // cores, nmax // cores)) for k in range(cores)]
+    for p in processes:
+        p.start()
--- logs.py
+++ logs.py
@@ -1,1 +1,49 @@
-# TODO : Split the simul dumping and simul loading into two different scripts for clarity.
+from matplotlib import pyplot as plt
+import os
+import pickle
+
+
+def end_numbers(string):
+    r = ""
+    for c in string[::-1]:
+        if c in "123456789.":
+            r += c
+        else:
+            break
+    return r[::-1]
+
+
+try:
+    with open("Simulations.picl", 'rb') as f:
+        simuls = pickle.load(f)
+except (FileNotFoundError, EOFError):
+    simuls = {}
+    with open("Simulations.picl", 'wb+') as f:
+        pickle.dump(simuls, f)
+
+logdir = "D://Wordzee/logs/"
+
+for d in os.listdir(logdir):
+    if os.path.isdir(logdir+d):
+        for file in os.listdir(logdir+d):
+            if os.path.isfile(file) and (filename := file[:-4]) not in simuls:
+                with open(os.path.join(os.getcwd() + '/' + d + '/', file), 'r') as f:
+                    r = f.readlines()
+                    V = {}
+                    entry_name = filename
+                    end_of_params = list(map(lambda x: x[0], r)).index('b')
+                    for k in range(1, end_of_params):
+                        V["param" + str(k)] = end_numbers(r[k].strip('\n'))
+                        entry_name += '|' + V["param" + str(k)]
+                    try:
+                        p = int(r[-1][13:])
+                        w = eval(r[-2][-6:-1])
+                    except (ValueError, NameError, IndexError):
+                        print(f"Invalid value encountered in {file}, moving on...")
+                    else:
+                        V["points"] = p
+                        V["wordzee"] = w
+                    simuls[entry_name] = V
+
+with open("Simulations.picl", "wb+") as f:
+    pickle.dump(simuls, f)
